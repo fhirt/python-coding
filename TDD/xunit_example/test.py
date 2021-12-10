ff335676e0e7c902fad9db52a7b697920c0e656d
tiehir <fabio.hirt@tie.ch>
2021-12-11 00:52:59 +0100

    TestCase.setup()

--- TDD/xunit_example/test.py
+++ TDD/xunit_example/test.py
@@ -1,10 +1,16 @@
 from xunit import WasRun, TestCase
 
 class TestCaseTest(TestCase):
+    def setup(self):
+        self.test = WasRun("test_method")
+
     def test_running(self):
-        test = WasRun("test_method")
-        assert(not test.was_run)
-        test.run()
-        assert(test.was_run)
+        self.test.run()
+        assert(self.test.was_run)
+
+    def test_setup(self):
+        self.test.run()
+        assert(self.test.was_setup)
 
 TestCaseTest("test_running").run()
+TestCaseTest("test_setup").run()
--- TDD/xunit_example/xunit/base.py
+++ TDD/xunit_example/xunit/base.py
@@ -2,7 +2,11 @@ class TestCase:
     def __init__(self, name) -> None:
         self.name = name
 
+    def setup(self):
+        pass        
+
     def run(self):
+        self.setup()
         method = getattr(self, self.name)
         method()
 
@@ -11,6 +15,10 @@ class WasRun(TestCase):
     def __init__(self, name) -> None:
         self.was_run = False
         super().__init__(name)
+
+    def setup(self):
+        self.was_run = False
+        self.was_setup = True
 
     def test_method(self):
         self.was_run = True
