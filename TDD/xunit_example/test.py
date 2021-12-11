b70f79bd4bb8c08ebc9bb43fa7ff1ed65e96f2c4
tiehir <fabio.hirt@tie.ch>
2021-12-11 13:29:10 +0100

    TestResult.summary()

--- TDD/xunit_example/test.py
+++ TDD/xunit_example/test.py
@@ -1,9 +1,22 @@
 from xunit import WasRun, TestCase
 
 class TestCaseTest(TestCase):    
+    def setup(self):
+        self.test = WasRun("test_method")
+
     def test_template_method(self):
-        test = WasRun("test_method")
-        test.run()
-        assert(test.log == "setup test_method tear_down")
+        self.test.run()
+        assert(self.test.log == "setup test_method tear_down")
+
+    def test_result(self):
+        result = self.test.run()
+        assert(result.summary() == "1 run, 0 failed")
+
+    def test_failed_result(self):
+        broken_test = WasRun("broken_method")
+        result = broken_test.run()
+        assert(result.summary() == "1 run, 1 failed")
 
 TestCaseTest("test_template_method").run()
+TestCaseTest("test_result").run()
+TestCaseTest("test_failed_result").run()
--- TDD/xunit_example/xunit/base.py
+++ TDD/xunit_example/xunit/base.py
@@ -7,16 +7,29 @@ class TestCase:
         optional setup before each test method
         """       
 
-    def run(self):
+    def run(self) -> "TestResult":
+        test_result = TestResult()
+        test_result.test_started()
         self.setup()
         method = getattr(self, self.name)
         method()
         self.tear_down()
+        return test_result
 
     def tear_down(self):
         """
         optional tear down after each test method
         """
+
+class TestResult:
+    def __init__(self) -> None:
+        self.run_count = 0
+
+    def test_started(self):
+        self.run_count += 1
+
+    def summary(self) -> str:
+        return f"{self.run_count} run, 0 failed"
 
 class WasRun(TestCase):
     def __init__(self, name) -> None:
@@ -30,3 +43,6 @@ class WasRun(TestCase):
 
     def tear_down(self):
         self.log = self.log + "tear_down"
+
+    def broken_method(self):
+        raise Exception
