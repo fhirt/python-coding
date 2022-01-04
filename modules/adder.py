6a5a16211e8b388130d656b2a4d7667136f426c7
fhirt <tiehir@tie.ch>
2022-01-04 21:59:24 +0100

    modules

---
+++ modules/adder.py
@@ -0,0 +1,5 @@
+def add(x, y):
+    return x + y
+
+def double(x):
+    return x + x
---
+++ modules/main.py
@@ -0,0 +1,5 @@
+from adder import add, double
+
+value = add(2, 3)
+double_value = double(value)
+print(double_value)
